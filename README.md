9cea7140ce2dabc35ddf4c34dcbf5a4f192a803d
Orne Brocaar <info@brocaar.com>
2016-01-03 14:15:49 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# LoRaBee terminal
+
+This is a simple terminal interface to write commands to and read responses
+from the LoRaBee (RN2483: http://shop.sodaq.com/en/lorabee-rn2483.html). It
+was tested in combination with the UARTSBee (http://shop.sodaq.com/en/uartsbee.html).
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/jacobsa/go-serial/serial"
+)
+
+func run(c *cli.Context) {
+	options := serial.OpenOptions{
+		PortName:        c.String("port-name"),
+		BaudRate:        57600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer port.Close()
+
+	go read(port)
+	write(port)
+}
+
+func read(r io.Reader) {
+	for {
+		if _, err := io.Copy(os.Stdout, r); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func write(w io.Writer) {
+	for {
+		fmt.Printf("\nEnter command: ")
+		in := bufio.NewReader(os.Stdin)
+		line, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmd := fmt.Sprintf("%s\r\n", strings.TrimSpace(line))
+		if _, err := w.Write([]byte(cmd)); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "lorabeeterminal"
+	app.Usage = "terminal to read and write from / to the LoRaBee"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "port-name",
+			Value: "/dev/tty.usbserial-AH02ZDDI",
+		},
+	}
+	app.Action = run
+	app.Run(os.Args)
+}
